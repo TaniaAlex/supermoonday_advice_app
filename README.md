7ca99562b8ef33c3b95a7d7f99460a63c2699403
TaniaAlex <2tatyanan@gmail.com>
2020-04-09 16:16:05 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+<!-- This Project is using Advice Slip JSON API -->
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { GiMoonClaws } from "react-icons/gi";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    advice: "",
+  };
+  componentDidMount() {
+    console.log("component did mount");
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((responce) => {
+        // destructuring responce data
+        const { advice } = responce.data.slip;
+        console.log(advice);
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    // destructuring
+    const { advice } = this.state;
+    return (
+      <div className="hero">
+        <nav className="navbar">
+          <div className="nav-center">
+            <div className="nav-header">
+              {/* <h3>
+                Super{"{Moon"}
+                {"}"}Day Advice
+              </h3> */}
+              <GiMoonClaws className="nav-icon" />
+              <h3>SuperMoonDayAdvice</h3>
+              {/* <span></span> */}
+            </div>
+          </div>
+        </nav>
+        <div className="banner">
+          <h1>{advice}</h1>
+          <div></div>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>I need advice</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
